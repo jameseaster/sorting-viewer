5cf4180faa3f45ba70d240dbebb4bf6339deffe0
James Easter <james.m.easter@gmail.com>
2023-03-16 21:40:58 -0600

    Update selection sort algorithm.

--- src/algorithms/selectionSort/selectionSort.js
+++ src/algorithms/selectionSort/selectionSort.js
@@ -5,9 +5,9 @@ export default async function selectionSort(ref, array) {
     for (let i = currentIdx + 1; i < array.length; i++) {
       // highlight only the values that are being compared
       let { value } = array[currentIdx];
-      ref.$set(array, currentIdx, { value, color: ref.compare });
+      ref.numbers[currentIdx] = { value, color: ref.compare };
       let { value: iIdx } = array[i];
-      ref.$set(array, i, { value: iIdx, color: ref.compare });
+      ref.numbers[i] = { value: iIdx, color: ref.compare };
 
       // pauses the event loop to better visualize the algo
       await new Promise((resolve) => setTimeout(resolve, 20));
@@ -16,17 +16,17 @@ export default async function selectionSort(ref, array) {
       if (array[i].value < array[smallestIdx].value) {
         // change the number at the smallest index to the primary color
         let { value } = array[smallestIdx];
-        ref.$set(array, smallestIdx, { value, color: ref.primary });
+        ref.numbers[smallestIdx] = { value, color: ref.primary };
         // smallest index now equals i
         smallestIdx = i;
         // Highlight the new smallest with gold
         let { value: iValue } = array[i];
-        ref.$set(array, i, { value: iValue, color: "gold" });
+        ref.numbers[i] = { value: iValue, color: "gold" };
       }
 
       if (smallestIdx !== i) {
         // Change comparison color back to primary to highlight the next comparison
-        ref.$set(array, i, { value: iIdx, color: ref.primary });
+        ref.numbers[i] = { value: iIdx, color: ref.primary };
       }
     }
 
@@ -34,12 +34,12 @@ export default async function selectionSort(ref, array) {
       // Swap values and set sorted color
       let { value: a } = array[smallestIdx];
       let { value: b } = array[currentIdx];
-      ref.$set(array, smallestIdx, { value: b, color: ref.primary });
-      ref.$set(array, currentIdx, { value: a, color: ref.sorted });
+      ref.numbers[smallestIdx] = { value: b, color: ref.primary };
+      ref.numbers[currentIdx] = { value: a, color: ref.sorted };
     }
     // Set sorted color even if smallesIdx === currentIdx
     let { value } = array[currentIdx];
-    ref.$set(array, currentIdx, { value, color: ref.sorted });
+    ref.numbers[currentIdx] = { value, color: ref.sorted };
 
     currentIdx += 1;
   }

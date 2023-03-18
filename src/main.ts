f1212950d8a7b46ef97dd88e082e8a3e68cb0a73
James Easter <james.m.easter@gmail.com>
2023-03-18 16:36:42 -0600

    Theme bug fix.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import "./assets/main.css";
 
 // PrimeVue
 import PrimeVue from "primevue/config";
+import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 import ToastService from "primevue/toastservice";
